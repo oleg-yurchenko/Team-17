295f426f4081fadf07a4dd333438f723358d7b0f
Oleg Yurchenko <oleg@bgmot.com>
2023-11-07 20:46:04 -0800

    Moved some files around, and created a testing area with finalzied movement script. Player is now a prefab instantiated into the 'playground' scene with a tilemap editor.

--- Assets/Scripts/MovementPrototypeController.cs
+++ Assets/Scripts/MovementPrototypeController.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class MovementPrototypeController : MonoBehaviour
 {
     public float c_moveForceMultiplier;
+    public float c_airMoveForceMultiplier;
     public float c_jumpForceMultiplier;
     public float c_speedLimit;
     private Vector2 horizontalForce = new Vector2();
     private Vector2 verticalForce = new Vector2();
     private Rigidbody2D body;
-    private bool c_hasJumped;
+    private bool hasJumped;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        c_hasJumped = false;
+        hasJumped = false;
     }
 
     // Update is called once per frame
@@ -27,20 +28,21 @@ public class MovementPrototypeController : MonoBehaviour
     {
         horizontalForce = new Vector2();
         verticalForce = new Vector2();
+        float horizontalMultiplier = (hasJumped ? c_airMoveForceMultiplier : c_moveForceMultiplier);
         if (Input.GetKey("w"))
             verticalForce += Vector2.up * c_jumpForceMultiplier;
         if (Input.GetKey("a"))
-            horizontalForce += Vector2.left * c_moveForceMultiplier;
+            horizontalForce += Vector2.left * horizontalMultiplier;
         if (Input.GetKey("d"))
-            horizontalForce += Vector2.right * c_moveForceMultiplier;
+            horizontalForce += Vector2.right * horizontalMultiplier;
 
-        if (!c_hasJumped && verticalForce.magnitude > 0.0f) {
-                body.AddForce(verticalForce, ForceMode2D.Impulse);
-                c_hasJumped = true;
+        if (!hasJumped && verticalForce.magnitude > 0.0f) {
+            body.AddForce(verticalForce, ForceMode2D.Impulse);
+            hasJumped = true;
         } else if (body.velocity.y == 0.0f) {
-            if (body.velocity.x <= c_speedLimit)
-                body.AddForce(horizontalForce);
-            c_hasJumped = false;
+            hasJumped = false;
         }
+        if (body.velocity.x <= c_speedLimit)
+            body.AddForce(horizontalForce);
     }
 }
