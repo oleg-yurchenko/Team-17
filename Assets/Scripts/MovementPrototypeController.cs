d025dae8ec9b1f4252baafa265cf60f5f017d04e
Oleg Yurchenko <oleg@bgmot.com>
2024-02-04 15:17:34 -0800

    modified double jump, will need fix later

--- Assets/Scripts/MovementPrototypeController.cs
+++ Assets/Scripts/MovementPrototypeController.cs
@@ -9,11 +9,14 @@ public class MovementPrototypeController : MonoBehaviour
     public float c_airMoveForceMultiplier;
     public float c_jumpForceMultiplier;
     public float c_speedLimit;
+	private ChargeJumpScript chargeJump;
     private Vector2 horizontalForce = new Vector2();
     private Vector2 verticalForce = new Vector2();
     private Rigidbody2D body;
     public bool hasJumped;
-    private bool jumpTime;
+    private float jumpTime;
+	private bool isSlowing = false;
+	private float initialForce;
 
     public bool hasDoubleJumped;
 
@@ -21,14 +24,23 @@ public class MovementPrototypeController : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+		chargeJump = GetComponent<ChargeJumpScript>();
         hasJumped = false;
         jumpTime = 0.2f;
+		initialForce = c_moveForceMultiplier;
         hasDoubleJumped = false;
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown("w")) {
+		ReducePlayerSpeed();
+    }
+    //not sure how to implement this code in fixed update (input.getkeydown doesn't work very well)
+
+    // FixedUpdate used for physics calculations
+    void FixedUpdate()
+    {
+		if (Input.GetKey("w")) {
             verticalForce += Vector2.up * c_jumpForceMultiplier;
             Jump();
         }
@@ -36,12 +48,6 @@ public class MovementPrototypeController : MonoBehaviour
             hasJumped = false;
             hasDoubleJumped = false;
         }
-    }
-    //not sure how to implement this code in fixed update (input.getkeydown doesn't work very well)
-
-    // FixedUpdate used for physics calculations
-    void FixedUpdate()
-    {
         horizontalForce = new Vector2();
         verticalForce = new Vector2();
         float horizontalMultiplier = (hasJumped ? c_airMoveForceMultiplier : c_moveForceMultiplier);
@@ -60,10 +66,28 @@ public class MovementPrototypeController : MonoBehaviour
                 body.AddForce(verticalForce, ForceMode2D.Impulse);
                 hasJumped = true;
             } 
+			/*
             else if (!hasDoubleJumped) {
                 body.AddForce(verticalForce, ForceMode2D.Impulse);
                 hasDoubleJumped = true;
             }
+			*/
     }
+	public void ReducePlayerSpeed()
+	{
+		if (chargeJump.c_isCharging && !isSlowing)
+		{
+			c_moveForceMultiplier /= 2.0f;
+			Debug.Log("Slowing Player!");
+			
+			isSlowing = true; // Mark that the speed reduction has occurred
+		}
+
+		else if (!chargeJump.c_isCharging && isSlowing)
+		{
+			c_moveForceMultiplier = initialForce;
+			isSlowing = false;
+		}
+	}
 }
 // can think about increasing gravity at the height of the jump to make it 'feel' better
