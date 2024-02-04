c33ef5405d064184010daa9582e17221d5b8bb72
yunitram <martin7uy@gmail.com>
2024-02-04 23:32:17 +0800

    changed the movement mechanic to include double jump

--- Assets/Scripts/MovementPrototypeController.cs
+++ Assets/Scripts/MovementPrototypeController.cs
@@ -11,38 +11,58 @@ public class MovementPrototypeController : MonoBehaviour
     private Vector2 horizontalForce = new Vector2();
     private Vector2 verticalForce = new Vector2();
     private Rigidbody2D body;
-    private bool hasJumped;
+    public bool hasJumped;
+    private bool jumpTime;
+
+    public bool hasDoubleJumped;
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         hasJumped = false;
+        jumpTime = 0.2f;
+        hasDoubleJumped = false;
     }
 
     // Update is called once per frame
     void Update() {
-
+        if (Input.GetKeyDown("w")) {
+            verticalForce += Vector2.up * c_jumpForceMultiplier;
+            Jump();
+        }
+        if (body.velocity.y == 0.0f) {
+            hasJumped = false;
+            hasDoubleJumped = false;
+        }
     }
+    //not sure how to implement this code in fixed update (input.getkeydown doesn't work very well)
+
     // FixedUpdate used for physics calculations
     void FixedUpdate()
     {
         horizontalForce = new Vector2();
         verticalForce = new Vector2();
         float horizontalMultiplier = (hasJumped ? c_airMoveForceMultiplier : c_moveForceMultiplier);
-        if (Input.GetKey("w"))
-            verticalForce += Vector2.up * c_jumpForceMultiplier;
         if (Input.GetKey("a"))
             horizontalForce += Vector2.left * horizontalMultiplier;
         if (Input.GetKey("d"))
             horizontalForce += Vector2.right * horizontalMultiplier;
 
-        if (!hasJumped && verticalForce.magnitude > 0.0f) {
-            body.AddForce(verticalForce, ForceMode2D.Impulse);
-            hasJumped = true;
-        } else if (body.velocity.y == 0.0f) {
-            hasJumped = false;
-        }
         if (body.velocity.x <= c_speedLimit)
             body.AddForce(horizontalForce);
     }
+
+    void Jump() 
+    {
+        if (!hasJumped) {
+                body.AddForce(verticalForce, ForceMode2D.Impulse);
+                hasJumped = true;
+            } 
+            else if (!hasDoubleJumped) {
+                body.AddForce(verticalForce, ForceMode2D.Impulse);
+                hasDoubleJumped = true;
+            }
+    }
 }
+// can think about increasing gravity at the height of the jump to make it 'feel' better
