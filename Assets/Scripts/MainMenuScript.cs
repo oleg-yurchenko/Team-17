36f706c7d7760dda7bd8e5a86c63875dadc73f6b
Enzo <enzotanyen@gmail.com>
2024-03-09 20:53:09 -0800

    Added main menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject mainMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    
+    public void Resume()
+    {
+        mainMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        mainMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void PlayTutorial()
+    {
+        Resume();
+        SceneManager.LoadScene("TutorialLevel");
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Options menu opened");
+    }
+}
