5cb40e9f2e5651a98bbf7f5bead8e8d4568c99be
Matthew <mharyant@students.cs.ubc.ca>
2024-03-17 20:19:16 -0700

    fix dragon serpent bug

--- Assets/Prototyping/DragonChasing/MarkerManager.cs
+++ Assets/Prototyping/DragonChasing/MarkerManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,33 +9,27 @@ public class MarkerManager : MonoBehaviour
 	{
 		public Vector3 position;
 		public Quaternion rotation;
-		
-		public Marker(Vector3 position, Quaternion rotation) 
+
+		public Marker(Vector3 position, Quaternion rotation)
 		{
 			this.position = position;
 			this.rotation = rotation;
 		}
 	}
-	
+
 	public List<Marker> markerList = new List<Marker>();
-	
-	// Start is called before the first frame update
-	void Start()
-	{
-		
-	}
 
 	void FixedUpdate()
 	{
 		UpdateMarkerList();
 	}
-	
-	public void UpdateMarkerList() 
+
+	public void UpdateMarkerList()
 	{
 		markerList.Add(new Marker(transform.position, transform.rotation));
 	}
-	
-	public void ClearMarkerList() 
+
+	public void ClearMarkerList()
 	{
 		markerList.Clear();
 		UpdateMarkerList();
--- Assets/Prototyping/DragonChasing/SerpentDragonMovement.cs
+++ Assets/Prototyping/DragonChasing/SerpentDragonMovement.cs
@@ -22,23 +22,15 @@ public class SerpentDragonMovement : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		FollowPlayer();
+		StartCoroutine(FollowPlayer());
 	}
 	
 	private void InitDragon() 
 	{
 		headRb = head.GetComponent<Rigidbody2D>();
-		
-		Vector3 newPos = head.transform.localPosition;
-		for (int i = 0; i < body.Count; i++) 
-		{
-			float offset = (i + 1) * offsetBetweenBody;
-			newPos += new Vector3 (offset - newPos.x, 0.0f);
-			body[i].transform.localPosition -= newPos;
-		}
 	}
 	
-	private void FollowPlayer() 
+	private IEnumerator FollowPlayer() 
 	{
 		// float directionToPlayer
 		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -51,10 +43,15 @@ public class SerpentDragonMovement : MonoBehaviour
 		
 		for (int i = 0; i < body.Count; i++) 
 		{
-			MarkerManager markerManager = (i == 0) ? head.GetComponent<MarkerManager>() : body[i - 1].GetComponent<MarkerManager>();
-			body[i].transform.position = markerManager.markerList[0].position;
-			body[i].transform.rotation = markerManager.markerList[0].rotation;
+			MarkerManager markerManager = (i == 0) ? head.GetComponent<MarkerManager>() 
+			: body[i - 1].GetComponent<MarkerManager>();
+			
+			Vector3 bodyPos = body[i].transform.position;
+			Quaternion bodyRot = body[i].transform.rotation;
+			body[i].transform.position = Vector3.Lerp(bodyPos, markerManager.markerList[0].position, 0.2f);
+			body[i].transform.rotation = Quaternion.Lerp(bodyRot, markerManager.markerList[0].rotation, 0.2f);
 			markerManager.markerList.RemoveAt(0);
+			yield return new WaitForSeconds(0.1f);
 		}
 	}
 }
