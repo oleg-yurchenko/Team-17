d298a4d3c27f54b071d92965fd64c421ad867f96
Matthew <mharyant@students.cs.ubc.ca>
2024-03-31 15:40:23 -0700

    add a dragon rig and its scripts

--- Assets/Prototyping/DragonChasing/DragonScript.cs
+++ Assets/Prototyping/DragonChasing/DragonScript.cs
@@ -2,34 +2,29 @@ using UnityEngine;
 
 public class DragonScript : MonoBehaviour
 {
-	[SerializeField] Transform target;
-	[SerializeField] Rigidbody2D headRb;
-	[SerializeField] GameObject body;
+	[SerializeField] GameObject root;
 	[SerializeField] float speed;
 	[SerializeField] float amplitude;
 	[SerializeField] float frequency;
-	private Rigidbody2D rb;
-	private Vector2 movement;
 	private Vector2 originalPos;
-	private Vector3 directionToPlayer;
+	private Vector2 originalRootPos;
 	
 	// Start is called before the first frame update
 	void Start()
 	{
+		originalRootPos = root.transform.position;
 		originalPos = transform.position;
-		rb = GetComponent<Rigidbody2D>();
 	}
 
 	void Update() 
 	{
-		directionToPlayer = (target.transform.position - headRb.transform.position).normalized;
-		float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
-		headRb.rotation = Mathf.Clamp(angle, -70, 70);
 	}
 
 	void FixedUpdate()
 	{   
 		Vector2 sinMove = new Vector2(0.0f, Mathf.Sin(Time.time * frequency) * amplitude * Time.deltaTime);
-		transform.position = originalPos + sinMove + (Vector2) transform.right * speed * Time.time;
+		root.transform.position = originalRootPos + sinMove;
+		
+		transform.position = originalPos + (Vector2) transform.right * speed * Time.deltaTime;
 	}
 }
