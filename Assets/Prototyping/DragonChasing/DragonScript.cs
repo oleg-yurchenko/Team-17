4a5ea6332bcb24da6c365faca79e73e1318656c5
Oleg Yurchenko <oleg@bgmot.com>
2024-03-31 20:43:54 -0700

    Updated some player values and added dragon script to tutorial level

--- Assets/Prototyping/DragonChasing/DragonScript.cs
+++ Assets/Prototyping/DragonChasing/DragonScript.cs
@@ -14,19 +14,25 @@ public class DragonScript : MonoBehaviour
 	void FixedUpdate()
 	{
 		Vector3 distToPlayer = playerPos.position - transform.position;
+		Debug.Log(distToPlayer);
 		
 		// This condition speeds up the dragon so it can catches the player when the player is too far
 		if (distToPlayer.x > 27) 
 		{
 			forwardSpeed *= 3;
-		} 
+		}
+		else if (distToPlayer.x < 0)
+		{
+			forwardSpeed = 0;
+		}
 		else 
 		{
 			forwardSpeed = originSpeed;
 		}
 		// Calculate forward movement for the body
 		// NOTE: because of the dragon's default rotation 180, right and left is reversed
-		Vector3 forwardMovement = -transform.right * forwardSpeed * Time.deltaTime;
+		//Vector3 forwardMovement = -transform.right * forwardSpeed * Time.deltaTime;
+		Vector3 forwardMovement = distToPlayer.normalized * forwardSpeed * Time.deltaTime;
 
 		// Apply forward movement to the body
 		transform.position += forwardMovement;
