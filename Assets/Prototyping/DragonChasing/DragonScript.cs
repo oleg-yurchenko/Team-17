b401fb6002a9cb518bacfdbdc21a09fded5e07a9
Matthew <mharyant@students.cs.ubc.ca>
2024-03-31 17:49:34 -0700

    add better animation to dragon

--- Assets/Prototyping/DragonChasing/DragonScript.cs
+++ Assets/Prototyping/DragonChasing/DragonScript.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class DragonScript : MonoBehaviour
 {
-		[SerializeField] float forwardSpeed;
+	[SerializeField] float forwardSpeed;
+	[SerializeField] Transform playerPos;
+	private float originSpeed;
 
+	private void Start()
+	{
+		originSpeed = forwardSpeed;
+	}
 
-		void FixedUpdate()
+	void FixedUpdate()
+	{
+		Vector3 distToPlayer = playerPos.position - transform.position;
+		
+		// This condition speeds up the dragon so it can catches the player when the player is too far
+		if (distToPlayer.x > 27) 
 		{
-				// Calculate forward movement for the body
-				Vector3 forwardMovement = transform.right * forwardSpeed * Time.deltaTime;
-
-				// Apply forward movement to the body
-				transform.position += forwardMovement;
+			forwardSpeed *= 3;
+		} 
+		else 
+		{
+			forwardSpeed = originSpeed;
 		}
+		// Calculate forward movement for the body
+		// NOTE: because of the dragon's default rotation 180, right and left is reversed
+		Vector3 forwardMovement = -transform.right * forwardSpeed * Time.deltaTime;
+
+		// Apply forward movement to the body
+		transform.position += forwardMovement;
+	}
 }
