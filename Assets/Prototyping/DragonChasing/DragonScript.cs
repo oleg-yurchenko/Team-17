7369fb3b9396b1bccb0c3cd52eb6e8aeff4d95c1
Matthew <mharyant@students.cs.ubc.ca>
2024-03-31 17:17:21 -0700

    add animation to dragon rig

--- Assets/Prototyping/DragonChasing/DragonScript.cs
+++ Assets/Prototyping/DragonChasing/DragonScript.cs
@@ -2,29 +2,15 @@ using UnityEngine;
 
 public class DragonScript : MonoBehaviour
 {
-	[SerializeField] GameObject root;
-	[SerializeField] float speed;
-	[SerializeField] float amplitude;
-	[SerializeField] float frequency;
-	private Vector2 originalPos;
-	private Vector2 originalRootPos;
-	
-	// Start is called before the first frame update
-	void Start()
-	{
-		originalRootPos = root.transform.position;
-		originalPos = transform.position;
-	}
+		[SerializeField] float forwardSpeed;
 
-	void Update() 
-	{
-	}
 
-	void FixedUpdate()
-	{   
-		Vector2 sinMove = new Vector2(0.0f, Mathf.Sin(Time.time * frequency) * amplitude * Time.deltaTime);
-		root.transform.position = originalRootPos + sinMove;
-		
-		transform.position = originalPos + (Vector2) transform.right * speed * Time.deltaTime;
-	}
+		void FixedUpdate()
+		{
+				// Calculate forward movement for the body
+				Vector3 forwardMovement = transform.right * forwardSpeed * Time.deltaTime;
+
+				// Apply forward movement to the body
+				transform.position += forwardMovement;
+		}
 }
